80cc24084e84038691639d6eb5e9d0bafe72d582
gt4eg43f43 <70478422+rajnarock@users.noreply.github.com>
2024-11-18 12:51:09 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 module.exports = {
-    token: "Your_Token",
+    token: "dQw4w9WgXcQ:djEwXpkMZ/Eo+8tqw0VWTbu4FOsHXFHrr+b9R+8dmcfCA/oHfCqqLT91/K/7FsXB5L1lVTHcq+PerPBGhx5Is8hlwxnxws6MmzQbG4gZmdXlSgzd6lP/1aQ/USIg0GzTfGP2uty7lA==",
     prefix: "!",
     allowedUserIDs: ['yourid1', 'yourid2', 'yourid3']
 };
